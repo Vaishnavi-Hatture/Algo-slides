7d43ef5c1661fbeb400a191d5cf2bf52ec6134a0
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-07-30 20:24:50 +0530

    feat: add algorithm card click handling

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask , render_template
 
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+   lago_name=""
+   
+   return render_template('index.html')
 
 
 
